a54f54fabf6027317e85c6cfdd5853d0c19de3f4
bryce <bryce@i.tv>
2012-07-03 11:26:32 -0600

    moved files around

---
+++ ITVAlphabetCoreDataTableView.h
@@ -0,0 +1,13 @@
+//
+//  ITVAlphabetCoreDataTableView.h
+//  AlphabetTable
+//
+//  Created by Bryce Redd on 7/3/12.
+//  Copyright (c) 2012 Itv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITVAlphabetCoreDataTableView : UITableView
+
+@end
---
+++ ITVAlphabetCoreDataTableView.m
@@ -0,0 +1,13 @@
+//
+//  ITVAlphabetCoreDataTableView.m
+//  AlphabetTable
+//
+//  Created by Bryce Redd on 7/3/12.
+//  Copyright (c) 2012 Itv. All rights reserved.
+//
+
+#import "ITVAlphabetCoreDataTableView.h"
+
+@implementation ITVAlphabetCoreDataTableView
+
+@end
